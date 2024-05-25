054580a237a1131f108a05983cbaad7e0617be47
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-26 01:12:16 +0900

    🛠️refactor: Test 환경 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import Modal from "./lib/components/Modal/Modal";
+
 const App = () => {
-  return <section></section>;
+  return (
+    <>
+      <Modal />
+    </>
+  );
 };
 
 export default App;
