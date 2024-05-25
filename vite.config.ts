c60cffd425c9df4fa3499f9f7b1613fda8680818
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-26 00:48:11 +0900

    🔨chore: prettier 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
 import * as path from "path";
+import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
