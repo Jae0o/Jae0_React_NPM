9666debd531e0cb7a739e669c02d1183f7f7346f
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-26 00:10:51 +0900

    🪄change: useSessionStorage JSDoc 수정

--- src/lib/hooks/useSessionStorage/useSessionStorage.ts
+++ src/lib/hooks/useSessionStorage/useSessionStorage.ts
@@ -5,6 +5,34 @@ import {
   UseSessionStorageProps,
 } from "./useSessionStorage.type";
 
+/**
+ * @function useSessionStorage
+ *
+ * Session Storage에 쉽게 저장하고 데이터를 꺼내기 위한 hook 입니다.
+ *
+ * @param key
+ *
+ * Session Storage에 접근할 `key` 값을 전달받습니다.
+ *
+ * @param initial
+ *
+ * 전달한 `key`에 해당하는 Session Storage의 기본 값을 전달받습니다.
+ *
+ * <hr>
+ *
+ * @returns data : T
+ *
+ * 전달한 `key`에 해당하는 Session Storage 값을 사용할 수 있습니다.
+ *
+ * @returns setSessionStorage : ( newData : T ) => T
+ *
+ * 전달한 `key`에 해당하는 Session Storage에 값을 추가합니다.
+ *
+ * @returns removeSessionStorage : () => void
+ *
+ * 전달한 `key`에 해당하는 Session Storage를 제거합니다.
+ *
+ */
 const useSessionStorage: UseSessionStorage = <T>({
   key,
   initialData,
