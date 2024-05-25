5711af7ff6905b4b4ed4cb197c1f46c0cda61a25
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-26 00:11:28 +0900

    🪄change: useToggle JSDoc 수정

--- src/lib/hooks/useToggle/useToggle.ts
+++ src/lib/hooks/useToggle/useToggle.ts
@@ -1,15 +1,23 @@
 import { useCallback, useState } from "react";
 
 /**
+ * @function useToggle
+ *
  * true false 두개의 상태를 toggle할 수 있는 hooks입니다.
  *
  * @param defaultValue useToggle의 기본값을 전달할 수 있습니다.
  *
- * @function handleToggle isToggle의 상태를 변경합니다.
+ * @return handleToggle : () => void
  *
- * @function handleToggleOn isToggle의 상태를 true로 변경합니다.
+ *  isToggle의 상태를 변경합니다.
  *
- * @function handleToggleOff isToggle의 상태를 false로 변경합니다.
+ * @return handleToggleOn : () => void
+ *
+ * isToggle의 상태를 true로 변경합니다.
+ *
+ * @return handleToggleOff : () => void
+ *
+ * isToggle의 상태를 false로 변경합니다.
  */
 const useToggle = (defaultValue: boolean = false) => {
   const [isToggle, setIsToggle] = useState<boolean>(defaultValue);
