a02a03f3ff1e1f91ef6d3a152d0ba25efaf4a57c
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-26 12:33:28 +0900

    🪄change: prettier 수정 반영

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,62 @@
 import React from "react";
 
 import Modal from "./lib/components/Modal/Modal";
+import useModal from "./lib/hooks/useModal/useModal";
 
 const App = () => {
+  const { isShow, handleShowModal, handleCloseModal } = useModal();
+  console.log(window.getComputedStyle(document.body).overflow);
   return (
     <>
-      <Modal />
+      <button onClick={handleShowModal}> 오픈</button>
+      <Modal
+        isShow={isShow}
+        onClose={handleCloseModal}
+      />
+
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          backgroundColor: "red",
+        }}
+      />
+      <button onClick={handleShowModal}> 오픈</button>
+
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          backgroundColor: "orange",
+        }}
+      />
+      <button onClick={handleShowModal}> 오픈</button>
+
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          backgroundColor: "yellow",
+        }}
+      />
+      <button onClick={handleShowModal}> 오픈</button>
+
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          backgroundColor: "green",
+        }}
+      />
+      <button onClick={handleShowModal}> 오픈</button>
+
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          backgroundColor: "blue",
+        }}
+      />
     </>
   );
 };
