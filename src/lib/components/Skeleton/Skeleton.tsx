683864b44a15fa1db9f52f952c7804a1218330e1
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-06-29 18:33:39 +0900

    💎feat: Skeleton 구현

--- src/lib/components/Skeleton/Skeleton.tsx
+++ src/lib/components/Skeleton/Skeleton.tsx
@@ -1,8 +1,14 @@
 import * as S from "./Skeleton.style";
 import { SkeletonProps } from "./Skeleton.type";
 
-const Skeleton = ({ width, height }: SkeletonProps) => {
-  return <div>Skeleton</div>;
+const Skeleton = ({ width, height, radius = "1.2rem" }: SkeletonProps) => {
+  return (
+    <S.SkeletonLayout
+      $width={width}
+      $height={height}
+      $radius={radius}
+    />
+  );
 };
 
 export default Skeleton;
