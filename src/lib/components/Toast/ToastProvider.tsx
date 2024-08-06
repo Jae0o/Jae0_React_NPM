fa2b6dc6ed5856cb1f3bd772bbb592ad62d4a837
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-08-06 18:40:53 +0900

    💎feat: Toast Store 연결

--- src/lib/components/Toast/ToastProvider.tsx
+++ src/lib/components/Toast/ToastProvider.tsx
@@ -1,11 +1,17 @@
 import { ToastProviderProps } from "./Toast.type";
 import * as S from "./ToastProvider.style";
+import { useToastStore } from "./stores";
 
 const ToastProvider = ({ children, zIndex = 50 }: ToastProviderProps) => {
+  const { toastList } = useToastStore();
   return (
     <>
       {children}
-      <S.ToastContainer $zIndex={zIndex} />
+      <S.ToastContainer $zIndex={zIndex}>
+        {toastList.map(({ id, message, deleteTime, type }) => (
+          <li>{id + message + deleteTime + type + "Test"}</li>
+        ))}
+      </S.ToastContainer>
     </>
   );
 };
