49f2979e1afe6b2ab1b67d3f2e4b11454adee336
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-08-16 12:06:51 +0900

    💎feat: Style Reset 관련 변수 생성

---
+++ src/lib/styles/resetStyle/resetStyle.ts
@@ -0,0 +1,11 @@
+import { css } from "styled-components";
+
+export const RESET_BUTTON = css`
+  background: inherit;
+  border: none;
+  box-shadow: none;
+  border-radius: 0;
+  padding: 0;
+  overflow: visible;
+  cursor: pointe;
+`;
